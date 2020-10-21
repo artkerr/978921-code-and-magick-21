6d57eb33057edb4de74ff188a9090df5b64be44d
Denis <d.y.vladimirov@gmail.com>
2020-10-21 14:00:53 +0300

    Модуль похожих персонажей + debounce

--- js/setup.js
+++ js/setup.js
@@ -1,17 +1,7 @@
 'use strict';
 
 (() => {
-  const COAT_COLOR = [`rgb(101, 137, 164)`, `rgb(241, 43, 107)`, `rgb(146, 100, 161)`, `rgb(56, 159, 117)`, `rgb(215, 210, 55)`, `rgb(0, 0, 0)`];
-  const EYES_COLOR = [`black`, `red`, `blue`, `yellow`, `green`];
-  const FIREBALL_COLOR = [`#ee4830`, `#30a8ee`, `#5ce6c0`, `#e848d5`, `#e6e848`];
-  const userDialog = document.querySelector(`.setup`);
-  const setupPlayer = userDialog.querySelector(`.setup-player`);
-  const wizardCoat = setupPlayer.querySelector(`.wizard-coat`);
-  const wizardEyes = setupPlayer.querySelector(`.wizard-eyes`);
-  const coatInput = setupPlayer.querySelector(`[name="coat-color"]`);
-  const eyesInput = setupPlayer.querySelector(`[name="eyes-color"]`);
-  const fireBallInput = setupPlayer.querySelector(`[name="fireball-color"`);
-  const fireBallColor = setupPlayer.querySelector(`.setup-fireball-wrap`);
+
   let coatColor = `rgb(101, 137, 164)`;
   let eyesColor = `black`;
   let wizards = [];
@@ -48,25 +38,20 @@
     }));
   };
 
-  wizardCoat.addEventListener(`click`, () => {
-    window.setWizardColor(wizardCoat, coatInput, COAT_COLOR);
-    coatColor = coatInput.value;
-    updateWizards();
+  window.wizard.setCoatChangeHandler((color) => {
+    coatColor = color;
+    window.debounce(updateWizards);
   });
 
-  wizardEyes.addEventListener(`click`, () => {
-    window.setWizardColor(wizardEyes, eyesInput, EYES_COLOR);
-    eyesColor = eyesInput.value;
-    updateWizards();
-  });
 
-  fireBallColor.addEventListener(`click`, () => {
-    window.setWizardColor(fireBallColor, fireBallInput, FIREBALL_COLOR);
+  window.wizard.setEyesChangeHandler((color) => {
+    eyesColor = color;
+    window.debounce(updateWizards);
   });
 
   const successHandler = (data) => {
     wizards = data;
-    updateWizards();
+    window.debounce(updateWizards);
   };
 
   const errorHandler = (errorMessage) =>{
@@ -75,6 +60,7 @@
 
   window.backend.load(successHandler, errorHandler);
 
+  const userDialog = document.querySelector(`.setup`);
   const form = userDialog.querySelector(`.setup-wizard-form`);
   form.addEventListener(`submit`, function (evt) {
     window.backend.save(new FormData(form), () => {
